b4a8db4249bc783688bef76dd952aaa569d9197b
Josué Yao <jyao2000@yahoo.fr>
2019-06-10 00:53:55 +0000

    bug fixe

--- PrintComb2.go
+++ PrintComb2.go
@@ -9,9 +9,9 @@ func PrintComb2()  {
 			for k:=0;k<10;k++{
 				for l:=0;l<10;l++{
 					nb1:= strconv.Itoa(i) + strconv.Itoa(j)
-					nb2:= strconv.Itoa(i) + strconv.Itoa(j)
+					nb2:= strconv.Itoa(k) + strconv.Itoa(l)
 					fmt.Print(nb1+nb2)
-					if(nb1< nb2){
+					if(nb1 < nb2){
 						fmt.Print(nb1 + " " +nb2)
 						if nb1 < "98" {
 							fmt.Print(", ")
