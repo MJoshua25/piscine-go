62f835946874a9c5aa5cb4d70fe0dfe9d52fa9e4
Josué Yao <jyao2000@yahoo.fr>
2019-06-10 00:26:42 +0000

    +1

--- PrintComb.go
+++ PrintComb.go
@@ -5,8 +5,8 @@ import "strconv"
 
 func PrintComb()  {
 	for i :=0; i<10; i++{
-		for j :=0; j<10; j++{
-			for k :=0; k<10; k++{
+		for j :=i+1; j<10; j++{
+			for k :=j+1; k<10; k++{
 				if(i!=j && i!= k && j!=k){
 					fmt.Print(strconv.Itoa(i)+strconv.Itoa(j)+strconv.Itoa(k)+", ")
 				}
