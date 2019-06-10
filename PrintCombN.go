ab832eb6f4856bc81e781a53339861dfcd5a8a9d
Josué Yao <jyao2000@yahoo.fr>
2019-06-10 15:14:02 +0000

    final

--- PrintCombN.go
+++ PrintCombN.go
@@ -1,14 +1,16 @@
 package piscine_go
 
 import "fmt"
+import "strconv"
 
-func printi(b int, n int, a int, i int)  {
+func printi(b int, n int, a int, i int,l string)  {
+	a++
 	for j:= b+1; j<10-n+a; j++{
-		fmt.Print(j)
-		if(a+1!=n){
-			a++
-			printi(j,n,a,i)
+		if(a<n){
+			printi(j,n,a,i,l+strconv.Itoa(j))
 		}else{
+			fmt.Print(l)
+			fmt.Print(j)
 			if i<10-n{
 				fmt.Print(", ")
 			} else{
@@ -19,11 +21,11 @@ func printi(b int, n int, a int, i int)  {
 }
 
 func PrintCombN(n int)  {
-	for i:=0; i<10-n; i++{
-		fmt.Print(i)
+	for i:=0; i<10; i++{
 		if n>1{
-			printi(i,n,1,i)
+			printi(i,n,1,i,strconv.Itoa(i))
 		} else{
+			fmt.Print(i)
 			if i<10-n{
 				fmt.Print(", ")
 			} else{
