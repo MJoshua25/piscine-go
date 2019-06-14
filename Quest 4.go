3c3e290645c2d025aa8326f7fcd5a0cff4978278
Josué Yao <jyao2000@yahoo.fr>
2019-06-14 05:37:40 +0000

    Récursive error

--- Quest 4.go
+++ Quest 4.go
@@ -21,7 +21,9 @@ func IterativeFactorial(nb int) int{
 }
 
 func RecursiveFactorial(nb int) int{
-	if nb <=1{
+	if nb>16 || nb<0{
+		return 0
+	} else if nb <=1{
 		return 1
 	} else{
 		return nb * RecursiveFactorial(nb -1)
