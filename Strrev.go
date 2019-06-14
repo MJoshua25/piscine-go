693756c6deb1e73038d9938968e254142857c7d4
Josué Yao <jyao2000@yahoo.fr>
2019-06-14 03:16:53 +0000

    StrRev

--- Strrev.go
+++ Strrev.go
@@ -1,6 +1,6 @@
 package piscine_go
 
-func Strrev(s string) string{
+func StrRev(s string) string{
 	sc := []byte(s)
 	for i, j := 0, len(sc)-1; i < j; i, j = i+1, j-1 {
 		sc[i], sc[j] = sc[j], sc[i]
