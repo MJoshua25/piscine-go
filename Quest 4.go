d0ddc60e1ff4498a0c3cf508e1fa1a7f783c6a55
Josué Yao <jyao2000@yahoo.fr>
2019-06-14 05:47:23 +0000

    fibo negative

--- Quest 4.go
+++ Quest 4.go
@@ -62,7 +62,7 @@ func Fibonacci(index int) int {
 	if index<2 && index>=0 {
 		return index
 	} else if index<0{
-		return 0
+		return -1
 	} else {
 		return Fibonacci(index -1) + Fibonacci(index -2)
 	}
