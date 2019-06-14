247ff1c992a96223948268bf11f0727a28e9d9cf
Josué Yao <jyao2000@yahoo.fr>
2019-06-14 05:46:46 +0000

    fibo 0

--- Quest 4.go
+++ Quest 4.go
@@ -59,8 +59,10 @@ func RecursivePower(nb, power int) int{
 }
 
 func Fibonacci(index int) int {
-	if index<2 {
+	if index<2 && index>=0 {
 		return index
+	} else if index<0{
+		return 0
 	} else {
 		return Fibonacci(index -1) + Fibonacci(index -2)
 	}
