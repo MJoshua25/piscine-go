e1f25da475dd8853c81677142ffee7fb150dcef6
Josué Yao <jyao2000@yahoo.fr>
2019-06-15 12:24:24 +0000

    Quest 5 part 2

--- Quest5.go
+++ Quest5.go
@@ -25,3 +25,54 @@ func Compare (a,b string) int{
 	return strings.Compare(a,b)
 }
 
+func ToUpper(s string)  string{
+	return strings.ToUpper(s)
+}
+
+func ToLower(s string)  string{
+	return strings.ToLower(s)
+}
+
+func Capitalize(s string)  string{
+	return strings.Title(s)
+}
+
+func IsAlpha(str string) bool {
+	alpha:= "abcdefghijklmnopqrstuvwxyz0123456789"
+	for _, char := range str{
+		if !strings.Contains(alpha, ToLower(string(char))){
+			return false
+		}
+	}
+	return true
+}
+
+func IsNumeric(str string) bool {
+	num:= "0123456789"
+	for _, char := range str{
+		if !strings.Contains(num, string(char)){
+			return false
+		}
+	}
+	return true
+}
+
+func IsLower(str string) bool {
+	alpha:= "abcdefghijklmnopqrstuvwxyz"
+	for _, char := range str{
+		if !strings.Contains(alpha, string(char)){
+			return false
+		}
+	}
+	return true
+}
+
+func IsUpper(str string) bool {
+	alpha:= ToUpper("abcdefghijklmnopqrstuvwxyz")
+	for _, char := range str{
+		if !strings.Contains(alpha, string(char)){
+			return false
+		}
+	}
+	return true
+}
