2fce0011277f457e3cf2f8766047ac55c64759f7
Josué Yao <jyao2000@yahoo.fr>
2019-06-15 11:53:10 +0000

    Quest 5 part 1

---
+++ Quest5.go
@@ -0,0 +1,28 @@
+package piscine_go
+
+import "fmt"
+import "strings"
+
+func FirstRune(s string) rune{
+	r := []rune(s)
+	return r[0]
+}
+
+func NRune(s string, n int) rune{
+	r := []rune(s)
+	return r[n]
+}
+
+func LastRune(s string) rune{
+	r := []rune(s)
+	return r[len(r)-1]
+}
+
+func Index(s, toFind string) int {
+	return strings.Index(s,toFind)
+}
+
+func Compare (a,b string) int{
+	return strings.Compare(a,b)
+}
+
